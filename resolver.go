2733bf926ae9eba42816cbfa7d07b41ad19ad652
andrew <0x9171@gmail.com>
2020-10-06 14:12:13 +0300

    upd

--- resolver.go
+++ resolver.go
@@ -1,50 +1,33 @@
 package resolver
 
 import (
-	"bufio"
 	"context"
 	"errors"
-	"io"
-	"math/rand"
+	"github.com/zofan/go-slist"
 	"net"
-	"net/http"
-	"strings"
 	"sync"
 	"time"
 )
 
 const (
-	ModeRandom SelectMode = 1
-	ModeRotate SelectMode = 2
-	ModeTime   SelectMode = 3
-
 	ServerListURL = `https://public-dns.info/nameservers.txt`
 	addressSuffix = `:53`
 )
 
 var (
-	ErrServerListEmpty = errors.New(`resolver: server list is empty`)
-	ErrBadMode         = errors.New(`resolver: bad mode`)
-	ErrRetryLimit      = errors.New(`resolver: retry limit`)
-	ErrNoSuchHost      = errors.New(`resolver: host not found`)
+	ErrRetryLimit = errors.New(`resolver: retry limit`)
+	ErrNoSuchHost = errors.New(`resolver: host not found`)
 )
 
-type SelectMode int
-
 type Resolver struct {
-	servers    []*Server
-	badServers []*Server
-	srvTicker  *time.Ticker
-	lastNS     uint32
+	Servers *slist.List
 
 	DialTimeout time.Duration
 	MaxFails    uint32
-	Mode        SelectMode
 	RetryLimit  int
 	RetrySleep  time.Duration
 	CacheLimit  int
 	CacheLife   time.Duration
-	RestoreTime time.Duration
 
 	cache map[string]cacheHost
 
@@ -56,104 +39,21 @@ type cacheHost struct {
 	lastHit time.Time
 }
 
-type Server struct {
-	Addr      string
-	Fails     uint32
-	LastUsage time.Time
-}
-
 func New() *Resolver {
 	r := &Resolver{
-		Mode:        ModeRotate,
 		DialTimeout: time.Second, // don't work, look at problem (net.dnsConfig.timeout - net/dnsconfig_unix.go:43)
 		RetryLimit:  5,
 		RetrySleep:  time.Millisecond * 500,
 		MaxFails:    30,
 		CacheLimit:  65535,
 		CacheLife:   time.Second * 300,
-		RestoreTime: time.Minute * 15,
+
+		Servers: slist.New(slist.ModeRotate, 3, time.Minute*10),
 
 		cache: make(map[string]cacheHost),
 	}
 
-	go func() {
-		for range time.Tick(time.Minute) {
-			r.restoreServers()
-		}
-	}()
-
 	return r
-}
-
-func (r *Resolver) LoadFromString(servers string) error {
-	return r.LoadFromReader(strings.NewReader(servers))
-}
-
-func (r *Resolver) LoadFromURL(url string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-
-	defer resp.Body.Close()
-
-	return r.LoadFromReader(resp.Body)
-}
-
-func (r *Resolver) LoadFromReader(reader io.Reader) error {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	r.servers = []*Server{}
-	r.badServers = []*Server{}
-	r.lastNS = 0
-
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		r.AddServer(strings.TrimSpace(scanner.Text()))
-	}
-
-	return scanner.Err()
-}
-
-func (r *Resolver) AddServer(addr string) {
-	ip := net.ParseIP(addr)
-	if ip == nil || !ip.IsGlobalUnicast() {
-		return
-	}
-
-	r.servers = append(r.servers, &Server{Addr: ip.String()})
-}
-
-func (r *Resolver) GetServers() []*Server {
-	return r.servers
-}
-
-func (r *Resolver) GetServer() (*Server, error) {
-	if len(r.servers) == 0 {
-		return nil, ErrServerListEmpty
-	}
-
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	switch r.Mode {
-	case ModeTime:
-		return r.servers[time.Now().Unix()%int64(len(r.servers))], nil
-	case ModeRandom:
-		return r.servers[rand.Intn(len(r.servers))], nil
-	case ModeRotate:
-		addr := r.servers[r.lastNS]
-
-		r.lastNS++
-		if int(r.lastNS) > len(r.servers)-1 {
-			r.lastNS = 0
-		}
-
-		return addr, nil
-	default:
-		return nil, ErrBadMode
-	}
 }
 
 func (r *Resolver) ResolveHost(host string) ([]net.IPAddr, error) {
@@ -221,12 +121,10 @@ func (r *Resolver) lookup(value string, fn func(*net.Resolver) error) error {
 	attempts := 1
 
 	for {
-		server, err := r.GetServer()
+		server, err := r.Servers.Get()
 		if err != nil {
 			return err
 		}
-
-		server.LastUsage = time.Now()
 
 		// todo: replace that to the custom dns client with proxy
 		stdR := &net.Resolver{
@@ -244,15 +142,13 @@ func (r *Resolver) lookup(value string, fn func(*net.Resolver) error) error {
 		err = fn(stdR)
 		{
 			if err, ok := err.(*net.DNSError); ok && err.IsNotFound {
-				server.Fails = 0
+				server.Good()
 				return ErrNoSuchHost
 			} else if err == nil {
-				server.Fails = 0
+				server.Good()
 				break
-			} else if server.Fails > r.MaxFails {
-				r.disableServer(server)
 			} else {
-				server.Fails++
+				r.Servers.MarkBad(server)
 			}
 		}
 
@@ -261,45 +157,12 @@ func (r *Resolver) lookup(value string, fn func(*net.Resolver) error) error {
 		}
 		attempts++
 
-		if len(r.servers) < 10 {
+		if r.Servers.Count() < 10 {
 			time.Sleep(r.RetrySleep)
 		}
 	}
 
 	return err
-}
-
-func (r *Resolver) restoreServers() {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	for i, s := range r.badServers {
-		if time.Since(s.LastUsage) > r.RestoreTime {
-			r.badServers[i] = r.badServers[len(r.badServers)-1]
-			r.badServers = r.badServers[:len(r.badServers)-1]
-
-			r.servers = append(r.servers, s)
-
-			s.Fails = 0
-		}
-	}
-}
-
-func (r *Resolver) disableServer(server *Server) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	for i, s := range r.servers {
-		if s == server {
-			r.servers[i] = r.servers[len(r.servers)-1]
-			r.servers = r.servers[:len(r.servers)-1]
-
-			r.badServers = append(r.badServers, s)
-
-			r.lastNS = 0
-			break
-		}
-	}
 }
 
 func (r *Resolver) clearCache() {
@@ -319,14 +182,4 @@ func (r *Resolver) clearCache() {
 			break
 		}
 	}
-}
-
-func (r *Resolver) Debug() map[string]interface{} {
-	d := make(map[string]interface{})
-
-	d[`servers`] = len(r.servers)
-	d[`badServers`] = len(r.badServers)
-	d[`cacheSize`] = len(r.cache)
-
-	return d
 }
--- resolver_test.go
+++ resolver_test.go
@@ -2,82 +2,15 @@ package resolver
 
 import (
 	"fmt"
+	"github.com/zofan/go-slist"
 	"sync"
 	"testing"
 )
 
-func TestGetServerRandom(t *testing.T) {
-	r := New()
-
-	err := r.LoadFromString("8.8.8.8\n1.1.1.1\n8.8.8.4")
-	if err != nil {
-		t.Error(err)
-	}
-
-	r.Mode = ModeRandom
-
-	srv1, err := r.GetServer()
-	if err != nil {
-		t.Error(err)
-	}
-	srv2, err := r.GetServer()
-	if err != nil {
-		t.Error(err)
-	}
-	if srv1 == srv2 {
-		t.Error(`two servers are the same, expecting different`)
-	}
-}
-
-func TestGetServerRoundRobin(t *testing.T) {
-	r := New()
-
-	err := r.LoadFromString("8.8.8.8\n1.1.1.1\n8.8.8.4")
-	if err != nil {
-		t.Error(err)
-	}
-
-	r.Mode = ModeRotate
-
-	servers := r.GetServers()
-
-	srv1, err := r.GetServer()
-	if err != nil {
-		t.Error(err)
-	}
-	if srv1 != servers[0] {
-		t.Error(`the first server expected ` + servers[0].Addr)
-	}
-
-	srv2, err := r.GetServer()
-	if err != nil {
-		t.Error(err)
-	}
-	if srv2 != servers[1] {
-		t.Error(`the second server expected ` + servers[1].Addr)
-	}
-
-	srv3, err := r.GetServer()
-	if err != nil {
-		t.Error(err)
-	}
-	if srv3 != servers[2] {
-		t.Error(`the third server expected ` + servers[2].Addr)
-	}
-
-	srv4, err := r.GetServer()
-	if err != nil {
-		t.Error(err)
-	}
-	if srv4 != servers[0] {
-		t.Error(`the fourth server expected ` + servers[0].Addr)
-	}
-}
-
 func TestResolveHost(t *testing.T) {
 	r := New()
 
-	err := r.LoadFromString("8.8.8.8")
+	err := r.Servers.LoadFromString("8.8.8.8")
 	if err != nil {
 		t.Error(err)
 	}
@@ -94,7 +27,7 @@ func TestResolveHost(t *testing.T) {
 func TestResolveNotExistsHost(t *testing.T) {
 	r := New()
 
-	err := r.LoadFromString("8.8.8.8")
+	err := r.Servers.LoadFromString("8.8.8.8")
 	if err != nil {
 		t.Error(err)
 	}
@@ -108,7 +41,7 @@ func TestResolveNotExistsHost(t *testing.T) {
 func TestReverseIP(t *testing.T) {
 	r := New()
 
-	err := r.LoadFromString("8.8.8.8\n1.1.1.1\n8.8.8.4")
+	err := r.Servers.LoadFromString("8.8.8.8\n1.1.1.1\n8.8.8.4")
 	if err != nil {
 		t.Error(err)
 	}
@@ -125,7 +58,7 @@ func TestReverseIP(t *testing.T) {
 func TestRetry(t *testing.T) {
 	r := New()
 
-	err := r.LoadFromString("1.0.0.0\n1.1.1.1\n2.0.0.0")
+	err := r.Servers.LoadFromString("1.0.0.0\n1.1.1.1\n2.0.0.0")
 	if err != nil {
 		t.Error(err)
 	}
@@ -143,7 +76,7 @@ func TestRetryLimit(t *testing.T) {
 	r := New()
 	r.RetryLimit = 2
 
-	err := r.LoadFromString("1.0.0.0\n2.0.0.0\n3.0.0.0")
+	err := r.Servers.LoadFromString("1.0.0.0\n2.0.0.0\n3.0.0.0")
 	if err != nil {
 		t.Error(err)
 	}
@@ -159,54 +92,13 @@ func TestRemoveBadServer(t *testing.T) {
 	r.RetryLimit = 0
 	r.MaxFails = 1
 
-	err := r.LoadFromString("1.0.0.0")
+	err := r.Servers.LoadFromString("1.0.0.0")
 	if err != nil {
 		t.Error(err)
 	}
 
 	_, err = r.ResolveHost(`google.com`)
-	if err != ErrServerListEmpty {
-		t.Error(err)
-	}
-}
-
-func TestBadServerList(t *testing.T) {
-	r := New()
-
-	err := r.LoadFromString("\n\nstring\n255.255.255.255\n127.0.0.1\n127.0.0.100\n0.0.0.0\n169.254.1.0\n169.254.254.255")
-	if err != nil {
-		t.Error(err)
-	}
-
-	if len(r.GetServers()) != 0 {
-		t.Error(`expected empty server list`)
-	}
-}
-
-func TestCache(t *testing.T) {
-	r := New()
-	r.CacheLimit = 2
-	r.CacheLife = 1
-
-	err := r.LoadFromString("8.8.8.8")
-	if err != nil {
-		t.Error(err)
-	}
-
-	_, err = r.ResolveHost(`google.com`)
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = r.ResolveHost(`google.com`)
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = r.ResolveHost(`yandex.com`)
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = r.ResolveHost(`facebook.com`)
-	if err != nil {
+	if err != slist.ErrServerListEmpty {
 		t.Error(err)
 	}
 }
@@ -218,7 +110,7 @@ func TestConcurrency(t *testing.T) {
 	r.MaxFails = 1
 	r.RetryLimit = 2
 
-	err := r.LoadFromURL(ServerListURL)
+	err := r.Servers.LoadFromURL(ServerListURL)
 	if err != nil {
 		t.Error(err)
 	}
@@ -239,9 +131,8 @@ func TestConcurrency(t *testing.T) {
 
 	go func() {
 		for i := 0; i < 10; i++ {
-			println(i)
 			_, err := r.ResolveHost(fmt.Sprintf(`abc-%d`, i) + `-yandex.com`)
-			if err != nil {
+			if err == nil {
 				t.Error(err)
 			}
 		}
